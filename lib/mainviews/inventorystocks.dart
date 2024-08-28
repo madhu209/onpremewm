4299e87ecbd191078df3d73b57478caaea132ddb
madhu209 <putta.msreddy@gmail.com>
2024-08-28 09:30:22 +0530

    inventory stock list design completed

--- lib/mainviews/inventorystocks.dart
+++ lib/mainviews/inventorystocks.dart
@@ -21,14 +21,45 @@ class InventoryStockListViewState extends State<InventoryStockListView> {
     return Scaffold(
       appBar: customAppBar('Warehouse:'),
       body: Padding(
-        padding: const EdgeInsets.only(left: 20, right: 20, top: 20),
+        padding: const EdgeInsets.only(left: 40, right: 40, top: 20),
         child: Column(
             mainAxisAlignment: MainAxisAlignment.start,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               productTtile(),
               const SizedBox(height: 30),
-              totalStockInfo()
+              totalStockInfo(true, 'Total Physical Stock', '1200',
+                  'Total Available Stock', '100'),
+              const SizedBox(height: 40),
+              const Divider(
+                color: Colors.black,
+              ),
+              SingleChildScrollView(
+                child: Column(
+                  children: List.generate(20, (index) {
+                    return Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const SizedBox(height: 10),
+                        const Text(
+                          'Goods ISSUE TITLE',
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 15,
+                              fontWeight: FontWeight.bold),
+                        ),
+                        const SizedBox(height: 15),
+                        totalStockInfo(false, 'Physical Stock', '1',
+                            'Available Stock', '1'),
+                        const SizedBox(height: 10),
+                        const Divider(
+                          color: Colors.black,
+                        )
+                      ],
+                    );
+                  }),
+                ),
+              )
             ]),
       ),
     );
@@ -48,36 +79,39 @@ class InventoryStockListViewState extends State<InventoryStockListView> {
     );
   }
 
-  Widget totalStockInfo() {
-    return const Row(
+  Widget totalStockInfo(bool isTotalStock, leftTitle, String leftValue,
+      String rightTitle, String rightValue) {
+    return Row(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
         Column(
           children: [
-            Text('Total Physical Stock'),
-            SizedBox(height: 20),
+            Text(leftTitle),
+            const SizedBox(height: 5),
             Text(
-              '1200',
-              style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+              leftValue,
+              style: TextStyle(
+                  fontSize: isTotalStock ? 25 : 15,
+                  fontWeight: FontWeight.bold),
             )
           ],
         ),
-        SizedBox(width: 50),
+        const Spacer(),
         Column(
           children: [
-            Text('Total Available Stock'),
-            SizedBox(
-              height: 20,
+            Text(rightTitle),
+            const SizedBox(
+              height: 5,
             ),
             Text(
-              '1100',
+              rightValue,
               style: TextStyle(
-                  fontSize: 25,
+                  fontSize: isTotalStock ? 25 : 15,
                   fontWeight: FontWeight.bold,
-                  color: Colors.green),
+                  color: isTotalStock ? Colors.green : Colors.black),
             )
           ],
-        )
+        ),
       ],
     );
   }
